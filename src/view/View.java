b464013aae74b46721c1345416d416b7d36aad5c
jdvp <jdrocks94@gmail.com>
2014-12-20 01:57:26 -0600

    very basic GUI

--- src/view/View.java
+++ src/view/View.java
@@ -1,13 +1,16 @@
 package view;
 
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by JD Porterfield on 12/18/2014.
  */
 public class View extends JFrame {
 
-    IV2MAdapter model = null;
+    private IV2MAdapter model = null;
+    private JPanel contactPanel;
+    private JPanel messagePanel;
 
     public View(IV2MAdapter adptIn) {
         model = adptIn;
@@ -20,5 +23,19 @@ public class View extends JFrame {
 
     public void initGUI() {
         setSize(600,450);
+        JScrollPane scroll = new JScrollPane();
+        getContentPane().add(scroll, BorderLayout.WEST);
+
+        contactPanel = new JPanel();
+        scroll.setViewportView(contactPanel);
+
+        JLabel lblContacts = new JLabel("Contacts:");
+        contactPanel.add(lblContacts);
+
+        JScrollPane scroll1 = new JScrollPane();
+        getContentPane().add(scroll1, BorderLayout.CENTER);
+
+        messagePanel = new JPanel();
+        scroll1.setViewportView(messagePanel);
     }
 }
